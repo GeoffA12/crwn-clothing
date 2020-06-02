4fa283222b3ba9d7e57e249cf8d70cabe724b856
arrgeoff <arrgeoff@amazon.com>
2020-06-02 17:34:13 -0500

    New sign in component and custom button component on the sign-in page.

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./sign-in-and-sign-up.style.scss";
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInAndSignUpPage = () => <SignIn />;
+
+export default SignInAndSignUpPage;
