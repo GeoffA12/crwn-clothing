29ab67610d790dd7056447b2a264c5b1740da566
arrgeoff <arrgeoff@amazon.com>
2020-06-02 15:47:51 -0500

    Added SVG and new router links to and from shop and homepage, new Header component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component.jsx";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     Switch gives us more control over our routing.  
     */
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
