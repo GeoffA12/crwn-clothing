a907d15391f6fff2a80a4ae0bc0535861ff6c0de
arrgeoff <arrgeoff@amazon.com>
2020-06-05 09:49:36 -0500

    Added checkout page, checkout item component and updated cart to use selectors.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,33 +2,41 @@ import React from "react";
 import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
-import { selectCartItems } from '../../redux/cart/cart.selectors';
-import { createStructuredSelector } from 'reselect';
-import { withRouter } from 'react-router-dom';
+import { selectCartItems } from "../../redux/cart/cart.selectors";
+import { createStructuredSelector } from "reselect";
+import { withRouter } from "react-router-dom";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import "./cart-dropdown.style.scss";
 
-const CartDropdown = ({ cartItems, history }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      
-      {
-      cartItems.length ? 
-      cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))
-      : 
-      <span className="empty-message">Your cart is empty</span>
-      }
-      <CustomButton onClick={() => history.push("/checkout")}>GO TO CHECKOUT</CustomButton>
+const CartDropdown = ({ cartItems, history, dispatch }) => {
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
+        <CustomButton
+          onClick={() => {
+            history.push("/checkout");
+            dispatch(toggleCartHidden());
+          }}
+        >
+          GO TO CHECKOUT
+        </CustomButton>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems
+  cartItems: selectCartItems,
 });
 
-// The order in which we wrap connect and withRouter matters. WithRouter needs to be the outer higher-order function because 
-// we need the connect component to evaluate first, which we'll then pass into the withRouter component. 
+// The order in which we wrap connect and withRouter matters. WithRouter needs to be the outer higher-order function because
+// we need the connect component to evaluate first, which we'll then pass into the withRouter component.
 export default withRouter(connect(mapStateToProps)(CartDropdown));
